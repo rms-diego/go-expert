a45422b659dbb53999a12daa9a2586352ea6ea0f
Diego Ramos <diegorbrito9@gmail.com>
2024-06-08 10:57:05 -0300

    [03 fundação] fim da aula 'importando fmt e tipagem'

---
+++ 3-fundacao/04-importando-fmt-e-tipagem/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+const name = "Diego"
+const age = 23
+
+func main() {
+	fmt.Printf("This variable call 'name' and our type is %T", name)
+	fmt.Printf("\nThis variable call 'age' and our type is %T", age)
+}
